853c145bb75b37ecc87bad0142fb1fd700cc3fc2
Elise Coutier <elise.coutier@mines-albi.fr>
2024-10-09 09:17:47 +0200

    feat: add gpx to map

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,36 +1,34 @@
 // Map on the homepage
-var map = L.map('mapid').setView([28.3949, 84.1240], 7); // Centre du Népal
+// var map = L.map('mapid').setView([28.3949, 84.1240], 7); // Centre du Népal
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '© OpenStreetMap contributors'
+// }).addTo(map);
 
 // Tour des Annapurnas - Besisahar à Bahundanda (Jour 1)
-if (document.getElementById('mapid1')) {
-    var map1 = L.map('mapid1').setView([28.2096, 83.9856], 10); // Coordonnées de l'Annapurna
+if (document.getElementById('mapid')) {
+    console.log('Carte détectée, initialisation...');
+
+    // Initialiser la carte
+    var map1 = L.map('mapid').setView([28.2096, 83.9856], 10); // Coordonnées de l'Annapurna
+    console.log('Carte initialisée avec les coordonnées de l\'Annapurna.');
+
+    // Charger les tuiles OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map1);
+    console.log('Tuiles OpenStreetMap chargées.');
 
-    // Ajout du fichier GPX jour 1
-    new L.GPX("static/gpx/activity_14588387021.gpx", {
+    // Charger le fichier GPX
+    var gpxFilePath = "static/gpx/activity_14588387021.gpx";
+    console.log('Tentative de chargement du fichier GPX : ' + gpxFilePath);
+
+    new L.GPX(gpxFilePath, {
         async: true
     }).on('loaded', function(e) {
+        console.log('Fichier GPX chargé avec succès.');
         map1.fitBounds(e.target.getBounds());
+    }).on('error', function(e) {
+        console.error('Erreur lors du chargement du fichier GPX : ', e);
     }).addTo(map1);
-}
-
-// Trek 1 - Annapurna (Jour 2)
-if (document.getElementById('mapid2')) {
-    var map2 = L.map('mapid2').setView([28.2096, 83.9856], 10);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map2);
-
-    // Ajout du fichier GPX jour 2
-    new L.GPX("static/gpx/trek1_day2.gpx", {
-        async: true
-    }).on('loaded', function(e) {
-        map2.fitBounds(e.target.getBounds());
-    }).addTo(map2);
 }
